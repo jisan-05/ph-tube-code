9264e27108b1bb7dc7c2883dad872af6a8b27588
Tanvir <zishantanvir05@gmail.com>
2024-10-04 11:45:28 +0600

    add verified badge

--- script/video.js
+++ script/video.js
@@ -21,22 +21,31 @@ const displayVideo = (videos) => {
     videos.forEach((video) => {
         console.log(video);
         const card = document.createElement("div");
-        card.classList = "card card-compact"
+        card.classList = "card card-compact";
         card.innerHTML = `
-        <figure>
+        <figure class="h-[200px] ">
     <img
       src=${video.thumbnail}
-      alt="Shoes" />
+      alt="Shoes" class="h-full w-full object-cover" />
   </figure>
-  <div class="card-body">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
+  <div class="px-0 py-2">
+    <div>
+        <img class="w-10 h-10 rounded-full object-cover" src=${video.authors[0].profile_picture} />
     </div>
+    <div>
+        <h2 class="font-bold">${video.title}</h2>
+        <div class="flex items-center">
+            <p>${video.authors[0].profile_name}</p>
+            ${video.authors[0].verified === true
+            ? `<img class="w-5 h-5 " src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png">`:''
+            }
+        </div>
+        <p> </p>
+    </div>
+    
   </div>
         `;
-        videoContainer.append(card)
+        videoContainer.append(card);
     });
 };
 
